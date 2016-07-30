2df79b1cd65d3e0686e05242db8cac0ccb4a727a
LeeUiHyeon <leeuihyeon71@gmail.com>
2016-07-30 23:53:04 +0900

    Update AIcode.js

--- AIcode.js
+++ AIcode.js
@@ -11,7 +11,7 @@ monster.prototype = {
   
 }
 
-    // 싸울 몬스터 생성
+    // 싸울 몬스터 종류(생성)
     var new_monster1 = new monster();
   
     new_monster1.name = "불꽃몬";
@@ -40,30 +40,43 @@ monster.prototype = {
     new_monster3.monnumber = 3;
 
 
-// AI 패턴
-function AI(){
+// AI 패턴 캡슐화로 진행
+function AI(t,m){
+    
+    var type = t;
+    var myhp = m;
+    
     
     // 몬스터 타입 확인
+    this.checktype = function(t){
+        if(t == 1){
+            console.log("불 type 패턴 시작");    
+            
+        }else if(t == 2){
+            console.log("풀 type 패턴 시작");
+        }else{
+            console.log("물 type 패턴 시작");
+        }
+        
+    }
     
-    
+    this.game = a ;
 
-};
+}; // AI
 
+// 몬스터의 공격 파악후 싸우는 턴 ++ 상승
 function fight(){
-    // 몬스터의 공격 파악후 싸우는 턴 ++ 상승
     
     // 싸우는 턴
     var turn = 0;
-    // 몬스터 행동 파악 (나중 스킬이 있을시)
-    var action = 0;
-    
-   
+
     // random으로 걸린 몬스터의 이름을 변수에 숫자를 넣어준다
     // 본인
     var mymon;
+    var myhp = new_monster(mymon).hp;
+    var mytype;
     // 상대
     var monname;
-    var myhp = new_monster(mymon).hp;
     var enemyhp = new_monster(monname).hp;
     // 싸움 결과
     var result = 0;
@@ -76,14 +89,15 @@ function fight(){
     if (turn % 2 == 0 ){
         document.write(new_monster(mymon) +"의 턴!");
         document.write(new_monster(mymon) +"의 공격!");
-        
-        document.write(new_monster(monname) +"은"+ (new_monster(monname).hp - new_monster(mymon).atk) + "의 데미지를 받았다");
+        damage = (new_monster(monname).hp - new_monster(mymon).atk);
+        document.write(new_monster(monname) +"은"+ damage + "의 데미지를 받았다");
         // 적 hp 종합 저장
-        enemyhp += (new_monster(monname).hp - new_monster(mymon).atk);
+        enemyhp -= damage;
         
-        // 화면상 체력 출력 코드 (미완성)
+        // 화면상 체력 출력 코드 (결합 예정)
         div.innerHTML = output;
         
+        // 끝난후 turn 상승
         turn ++ ;
         
         
@@ -93,14 +107,15 @@ function fight(){
         document.write(new_monster(monname).name + "의 턴!");
         
         document.write(new_monster(mymon) +"의 공격!");
-        // AI식 공격
-        AI();
+        // AI 공격
+        var AI_active = new AI(mytype, enemyhp);
+        // AI 공격 결과 
+        damage = AI_active;
         // 결과 보고
-        document.write(new_monster(mymon) +"은"+ (new_monster(mymon).hp - new_monster(monname).atk) + "의 데미지를 받았다");
+        document.write(new_monster(mymon) +"은"+ damage + "의 데미지를 받았다");
         // 적 hp 종합 저장
-        myhp += (new_monster(monname).hp - new_monster(mymon).atk);
+        myhp -= damage;
         
-        // 화면상 체력 출력 코드 (미완성)
         div.innerHTML = output;
         
         turn ++ ;
@@ -108,7 +123,7 @@ function fight(){
     }
     
     
-}
+} // fight
 
 
 
